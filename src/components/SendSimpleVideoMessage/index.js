222b42084f2728cba3529b4e3f7701dbe48d3a06
tabishabbasidev <tabishabbasidev@gmail.com>
2024-03-13 07:53:58 +0530

    Added functionality in SendSimpleVideoMessage component to ensure that user will select only video file if other it will notify by error notification by toast

--- src/components/SendSimpleVideoMessage/index.js
+++ src/components/SendSimpleVideoMessage/index.js
@@ -10,6 +10,7 @@ import { useState, useRef, useContext } from "react";
 import { MdSend } from "react-icons/md";
 import { v4 as uuidv4 } from "uuid";
 import { ChatContext } from "../Context/ChatContext";
+import { ToastContainer, toast } from "react-toastify";
 
 const messageTypeConstants = {
   text: "TEXT",
@@ -39,6 +40,14 @@ export default function SendSimpleVideoMessage({ onClose }) {
   const [base64Video, setBase64Video] = useState(null);
   const [apiStatus, setApiStatus] = useState(apiConstants.initial);
 
+  const toastOptions = {
+    autoClose: 2000,
+    style: {
+      background: "#0f172a",
+      color: "#fff",
+    },
+  };
+
   const { profile, setChatList, selectedChat, socket } =
     useContext(ChatContext);
 
@@ -48,14 +57,22 @@ export default function SendSimpleVideoMessage({ onClose }) {
     const file = e.target.files[0];
 
     if (file) {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
+      if (file.type.startsWith("video/")) {
+        if (file.size <= 100 * 1024 * 1024) {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
 
-      reader.onloadend = () => {
-        console.log("loading completed ");
-        setVideo(reader.result);
-        setBase64Video(reader.result.split(",")[1]);
-      };
+          reader.onloadend = () => {
+            console.log("loading completed ");
+            setVideo(reader.result);
+            setBase64Video(reader.result.split(",")[1]);
+          };
+        } else {
+          toast.error("File size should not exceed 100MB", toastOptions);
+        }
+      } else {
+        toast.error("Please select video file only.", toastOptions);
+      }
     }
   };
 
@@ -154,6 +171,7 @@ export default function SendSimpleVideoMessage({ onClose }) {
           </SendBtn>
         )}
       </ButtonsContainer>
+      <ToastContainer />
     </MainContainer>
   );
 }
