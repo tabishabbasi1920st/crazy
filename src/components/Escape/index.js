fafcb77bff02e9ac5e6a9f86d036887c0b5318d1
tabishabbasi1920st <tabishabbasi1920st@gmail.com>
2024-04-01 00:26:05 +0530

    Added content in escape route

--- src/components/Escape/index.js
+++ src/components/Escape/index.js
@@ -1,7 +1,61 @@
-import React from "react";
+import { MainContainer } from "./styledComponents";
 
 const index = () => {
-  return <div>Escape route</div>;
+  return (
+    <MainContainer>
+      <article>
+        <h2 style={{ marginBottom: "10px" }}>INTRODUCTION:</h2>
+        <p>
+          "Connect Me" is a versatile chatting application designed to
+          facilitate effortless communication between individuals or groups.
+          Whether you're catching up with an old friend, collaborating with team
+          members on a project, or simply sharing moments with loved ones,
+          Connect Me provides a reliable platform for instant messaging, voice
+          calls, and multimedia sharing.
+        </p>
+
+        <h2 style={{ marginBottom: "10px", marginTop: "10px" }}>Features:</h2>
+        <ol start="0" type="A">
+          <li>
+            <strong>Instant Messaging:</strong> At the core of Connect Me lies
+            its robust instant messaging feature, allowing users to exchange
+            text messages in real-time...
+          </li>
+          <li>
+            <strong>Voice and Video Calls:</strong> In addition to text
+            messaging, Connect Me offers seamless voice and video calling
+            capabilities...
+          </li>
+          <li>
+            <strong>Multimedia Sharing:</strong> Connect Me empowers users to
+            share multimedia content effortlessly...
+          </li>
+          <li>
+            <strong>Privacy and Security:</strong> Recognizing the importance of
+            privacy and security in digital communication...
+          </li>
+          <li>
+            <strong>Customization and Personalization:</strong> Connect Me
+            offers a plethora of customization options to suit individual
+            preferences and tastes...
+          </li>
+        </ol>
+
+        <h2 style={{ marginBottom: "10px", marginTop: "10px" }}>Impact</h2>
+        <p>
+          The widespread adoption of chatting applications like Connect Me has
+          transformed the way we communicate...
+        </p>
+
+        <h2 style={{ marginBottom: "10px", marginTop: "10px" }}>Conclusion</h2>
+        <p>
+          As we navigate the complexities of modern-day communication, chatting
+          applications like Connect Me serve as invaluable tools for staying
+          connected, informed, and engaged...
+        </p>
+      </article>
+    </MainContainer>
+  );
 };
 
 export default index;
---
+++ src/components/Escape/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  min-height: 100vh;
+  max-height: 100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-color: #132036;
+  color: #fff;
+  padding: 5px;
+
+  ol li {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+`;
